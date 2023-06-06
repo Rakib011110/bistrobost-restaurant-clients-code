50230b35bce1cae31c15e3c0d88c8e7b90d6a883
Rakibul Islam <rakib011110@gmail.com>
2023-06-06 16:01:39 +0600

    admin add

---
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AddItems = () => {
+  return (
+    <div>
+      <div className="text-4xl">
+        {" "}
+        <h1> Add Items</h1>
+      </div>
+    </div>
+  );
+};
+
+export default AddItems;
